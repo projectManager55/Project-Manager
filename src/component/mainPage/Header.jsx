cde7f8ef5ce694c3f6a2e12f3fa402ed24e91b05
AzatOmarov <kst369@mail.ru>
2019-12-22 20:19:31 +0100

    Change state of Header when user searches the Project

--- src/component/mainPage/Header.jsx
+++ src/component/mainPage/Header.jsx
@@ -3,13 +3,36 @@ import '../../assets/App.css';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class Header extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      projectName: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  }
+
   render() {
+    const { projectName } = this.state;
     return (
       <div className="header">
         <div className="header__greeting">
           <h2>Hi Azat</h2>
+          <p>Welcome back to the workspace, we missed you!</p>
         </div>
-        <input type="text" name="projects" className="header__input" />
+        <input
+          type="text"
+          className="header__input"
+          name="projectName"
+          value={projectName}
+          placeholder="Search Task or Project..."
+          onChange={this.handleChange}
+        />
       </div>
     );
   }
