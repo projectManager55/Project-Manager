154203d3b5aa64b3c26d72008700cafecffa827b
AzatOmarov <kst369@mail.ru>
2019-12-23 03:18:58 +0100

    Change Projects.jsx

--- src/container/projects/Projects.jsx
+++ src/container/projects/Projects.jsx
@@ -1,9 +1,39 @@
-import React, { Fragment } from 'react';
+import React from 'react';
+import Square from '../../component/mainPage/Square';
+
 import '../../assets/App.css';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class Project extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      projects: props.projects,
+    };
+  }
+
+  renderProjects = (projects) => {
+    if (projects.length > 0) {
+      return projects.map((p) => <Square project={p} key={p.id} selectItem={this.selectItem} />);
+    }
+    // return this.state.projects.map((p) => <Square project={p} key={p.id} selectItem={this.selectItem} />);
+  }
+
+  selectItem = (id) => {
+    this.setState((prevState) => {
+      const nProjects = prevState.projects.map((p) => {
+        p.selected = p.id === id;
+        return p;
+      });
+      return {
+        projects: nProjects,
+      };
+    });
+  }
+
   render() {
+    const { projects } = this.props;
+    const projectsList = this.renderProjects(projects);
     return (
       <>
         <div className="projects">
@@ -11,9 +41,7 @@ class Project extends React.Component {
             <p>Projects(12)</p>
           </div>
           <div className="projects-block">
-            <div className="projects-block__square">
-                ML
-            </div>
+            {projectsList}
           </div>
         </div>
       </>
