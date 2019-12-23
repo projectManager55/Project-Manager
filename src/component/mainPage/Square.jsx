61c667a7e10ccbb95f1cd5ccfb1fba04546a6b22
AzatOmarov <kst369@mail.ru>
2019-12-23 03:15:30 +0100

    Change Square.jsx component

--- src/component/mainPage/Square.jsx
+++ src/component/mainPage/Square.jsx
@@ -1,12 +1,24 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import '../../assets/App.css';
 
 function Square(props) {
-  const { projectName, color } = props;
-  const projectShortName = projectName.substring(0, 1);
+  const {
+    project, selectItem,
+  } = props;
+  const projectName = `${project.name.substring(0, 1)}A`;
   return (
-    <div className="projects-block__square" style={{ backgroundColor: color }}>
-      {projectShortName}
+    <div
+      className="projects-block__square"
+      style={{ border: project.selected ? `solid 2px ${project.color}` : '' }}
+      onClick={() => selectItem(project.id)}
+      role="button"
+      tabIndex="0"
+      onKeyDown={() => selectItem(project.id)}
+    >
+      <div className="projects-block__innerSquare" style={{ backgroundColor: project.color }}>
+        <span className="projects-block__project-title">{projectName}</span>
+      </div>
+
     </div>
   );
 }
