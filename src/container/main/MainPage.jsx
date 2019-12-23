db77443a770de084303f0beab88cda5d0bb9d661
AzatOmarov <kst369@mail.ru>
2019-12-23 03:17:55 +0100

    Change MainPage.jsx to render Dashboard and ProjectDetails

--- src/container/main/MainPage.jsx
+++ src/container/main/MainPage.jsx
@@ -1,19 +1,40 @@
 import React from 'react';
-import Header from '../../component/mainPage/Header';
-import Projects from '../projects/Projects';
 import '../../assets/App.css';
+import { projects } from '../../dummyData/dummyData';
+import Dashboard from '../dashboard/Dashboard';
+import ProjectDetails from '../projectDetails/ProjectDetails';
+
 
 // eslint-disable-next-line react/prefer-stateless-function
 class MainPage extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      localProjects: projects,
+      isItemSelected: false,
+    };
+  }
+
+  selectItem = (id) => {
+    this.setState((prevState) => {
+      const nProjects = prevState.localProjects.map((p) => {
+        p.selected = p.id === id;
+        return p;
+      });
+      return {
+        localProjects: nProjects,
+        isItemSelected: true,
+      };
+    });
+  }
+
+
   render() {
+    const { localProjects, isItemSelected } = this.state;
     return (
       <div className="main">
-        <div className="header-wrapper">
-          <Header />
-        </div>
-        <div className="projects-wrapper">
-          <Projects />
-        </div>
+        <Dashboard projects={localProjects} selectItem={this.selectItem} />
+        {!isItemSelected ? null : <ProjectDetails projects={localProjects} selectItem={this.selectItem} isItemSelected={isItemSelected} />}
       </div>
     );
   }
