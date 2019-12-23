230f3cf2f78babd68146e9f7ab303ea885fcb1e4
AzatOmarov <kst369@mail.ru>
2019-12-23 06:08:40 +0100

    Change ProjectDetails.jsx to render actual and upcoming events

--- src/container/projectDetails/ProjectDetails.jsx
+++ src/container/projectDetails/ProjectDetails.jsx
@@ -1,32 +1,41 @@
 import React from 'react';
 import Square from '../../component/mainPage/Square';
+import returnFormatedDate from '../../utils/returnDay';
 import '../../assets/App.css';
 
 class Dashboard extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      projectName: '',
-      projects: props.projects,
       isItemSelected: false,
     };
   }
 
   render() {
-    const { projects, isItemSelected } = this.state;
+    const { isItemSelected } = this.state;
+    let { projectId, events, projectName } = this.props;
+    events = events.filter((e) => e.id === projectId);
+    let currentEvents = events.filter((e) => returnFormatedDate(e.date) === returnFormatedDate(new Date()));
+    currentEvents = currentEvents.map((e) => <p>{e.description}</p>);
+    let upcomingEvents = events.filter((e) => new Date() < new Date(e.date));
+    upcomingEvents = upcomingEvents.map((e) => <p>{e.description}</p>);
     return (
       <div className={!isItemSelected ? 'active-window' : 'active-window_project-selected'}>
         <div className="project-details">
           <div className="project-details-wrapper">
-
             <div className="project-details-header">
               <div className="project-details-header__title">
-                <h2>Shopping Application</h2>
+                <h2>{projectName}</h2>
                 <p>Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?</p>
               </div>
             </div>
             <h4>Today</h4>
-            <p>all events</p>
+            {currentEvents}
+            <h4>Upcoming</h4>
+            {upcomingEvents}
+          </div>
+          <div className="project-details__button-wrapper">
+            <button type="button" onClick={() => console.info('open modal')} className="add-event-button">+</button>
           </div>
         </div>
       </div>
