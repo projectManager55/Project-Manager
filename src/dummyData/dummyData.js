e877743de3b187ed194bec7a968bcf67483520c8
AzatOmarov <kst369@mail.ru>
2019-12-23 03:16:27 +0100

    Create data file for internal use - dummyData.js

---
+++ src/dummyData/dummyData.js
@@ -0,0 +1,57 @@
+export const projects = [
+  {
+    name: 'Health App',
+    id: 0,
+    selected: false,
+    color: 'green',
+  },
+  {
+    name: 'Shopping App',
+    id: 1,
+    selected: false,
+    color: 'yellow',
+  },
+  {
+    name: 'MedMee',
+    id: 2,
+    selected: false,
+    color: 'pink',
+  },
+  {
+    name: 'Insurance',
+    id: 3,
+    selected: false,
+    color: 'purple',
+  },
+];
+
+export const events = [
+  {
+    id: 0,
+    title: 'Meeting',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat',
+    date: '23.12.2019',
+    team: 'Front end',
+  },
+  {
+    id: 0,
+    title: 'Product lunch',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat',
+    date: '23.12.2019',
+    team: 'Front end and Backend',
+  },
+  {
+    id: 1,
+    title: 'Teambuilding',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat',
+    date: '11.01.2020',
+    team: 'Front end',
+  },
+  {
+    id: 1,
+    title: 'Meeting',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat',
+    date: '11.07.2019',
+    team: 'Front end and Backend',
+  },
+];
